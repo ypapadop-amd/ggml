f1960d4a6a631648358566ba623c44e064ded232
Yiannis Papadopoulos <102817138+ypapadop-amd@users.noreply.github.com>
2024-12-04 13:47:51 -0500

    HSA backend initialization (#3)
    * CMake support for HSA backend
    
    * Stubs for initializing HSA
    
    * Stubs for HSA buffers
    
    * Stubs for HSA and host buffers
    
    * Using new backend CMake declaration
    
    * Additional stubs for HSA backend
    
    * Formatting
    
    * Adding function to track unimplemented APIs

--- src/ggml-backend-reg.cpp
+++ src/ggml-backend-reg.cpp
@@ -29,6 +29,10 @@
 #include "ggml-cpu.h"
 #endif
 
+#ifdef GGML_USE_HSA
+#include "ggml-hsa.h"
+#endif
+
 #ifdef GGML_USE_CUDA
 #include "ggml-cuda.h"
 #endif
@@ -178,6 +182,9 @@ struct ggml_backend_registry {
     std::vector<ggml_backend_dev_t> devices;
 
     ggml_backend_registry() {
+#ifdef GGML_USE_HSA
+        register_backend(ggml_backend_hsa_reg());
+#endif
 #ifdef GGML_USE_CUDA
         register_backend(ggml_backend_cuda_reg());
 #endif
