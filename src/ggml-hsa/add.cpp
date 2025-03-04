8c0564632248a576299d51a9838c8119df0ff7f6
Yiannis Papadopoulos <102817138+ypapadop-amd@users.noreply.github.com>
2025-03-03 22:41:30 -0500

    Clang format support (#26)
    * clang-format configuration loosely based on ggml-sycl
    
    * Formatting

--- src/ggml-hsa/add.cpp
+++ src/ggml-hsa/add.cpp
@@ -2,7 +2,8 @@
 
 #include "ggml-impl.h"
 
-bool ggml_hsa_supports_add(const ggml_hsa_device_info::device_info & dev_info, const ggml_tensor * tensor) {
+bool ggml_hsa_supports_add(const ggml_hsa_device_info::device_info & dev_info,
+                           const ggml_tensor * tensor) {
     const ggml_tensor * src0 = tensor->src[0];
     const ggml_tensor * src1 = tensor->src[1];
     const ggml_tensor * dst = tensor;
@@ -10,7 +11,7 @@ bool ggml_hsa_supports_add(const ggml_hsa_device_info::device_info & dev_info, c
     GGML_ASSERT(ggml_are_same_shape(src0, src1) && ggml_are_same_shape(src0, dst));
 
     if ((src0->type != src1->type) || (src0->type != dst->type) || src0->type != GGML_TYPE_I32) {
-      return false;
+        return false;
     }
 
     return ggml_hsa_kernel_exists(dev_info, tensor);
@@ -29,7 +30,8 @@ ggml_status ggml_hsa_add(ggml_backend_hsa_context & ctx, ggml_tensor * tensor) {
     const std::int64_t element_count = ggml_nelements(src0);
 
     ggml_hsa_aie_kernel kernel;
-    if (auto status = ggml_hsa_find_aie_kernel(ctx, tensor, kernel); status != GGML_STATUS_SUCCESS) {
+    if (auto status = ggml_hsa_find_aie_kernel(ctx, tensor, kernel);
+        status != GGML_STATUS_SUCCESS) {
         return status;
     }
 
@@ -37,13 +39,15 @@ ggml_status ggml_hsa_add(ggml_backend_hsa_context & ctx, ggml_tensor * tensor) {
 #define HIGH_ADDR(addr) (reinterpret_cast<uint64_t>(addr) >> 32)
 
     hsa_amd_aie_ert_start_kernel_data_t * cmd_payload = nullptr;
-    if (auto status = hsa_amd_memory_pool_allocate(dev_info.kernarg_memory.memory_pool, 64, 0, reinterpret_cast<void **>(&cmd_payload));
+    if (auto status = hsa_amd_memory_pool_allocate(dev_info.kernarg_memory.memory_pool, 64, 0,
+                                                   reinterpret_cast<void **>(&cmd_payload));
         status != HSA_STATUS_SUCCESS) {
-        GGML_LOG_ERROR("%s: Could not allocate hsa_amd_aie_ert_start_kernel_data_t (%d)\n", __func__, status);
+        GGML_LOG_ERROR("%s: Could not allocate hsa_amd_aie_ert_start_kernel_data_t (%d)\n",
+                       __func__, status);
         return GGML_STATUS_FAILED;
     }
     cmd_payload->pdi_addr = kernel.pdi_buffer.data; // PDI to use with this command
-    cmd_payload->data[0] = 0x3; // Transaction opcode
+    cmd_payload->data[0] = 0x3;                     // Transaction opcode
     cmd_payload->data[1] = 0x0;
     cmd_payload->data[2] = LOW_ADDR(kernel.insts_buffer.data);
     cmd_payload->data[3] = HIGH_ADDR(kernel.insts_buffer.data);
