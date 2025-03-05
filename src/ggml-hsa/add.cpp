14e28dc6fa5432330714e6c1e536d23caaa2607a
Yiannis Papadopoulos <102817138+ypapadop-amd@users.noreply.github.com>
2025-03-05 14:28:19 -0500

    Free packet memory after synchronization (#28)
    * Comments, disabling copy/move when not allowed
    
    * Replacing high / low bits macros
    
    * Factoring out dispatch functionality
    
    * Free all finished packets

--- src/ggml-hsa/add.cpp
+++ src/ggml-hsa/add.cpp
@@ -35,9 +35,6 @@ ggml_status ggml_hsa_add(ggml_backend_hsa_context & ctx, ggml_tensor * tensor) {
         return status;
     }
 
-#define LOW_ADDR(addr) (reinterpret_cast<uint64_t>(addr) & 0xFFFFFFFF)
-#define HIGH_ADDR(addr) (reinterpret_cast<uint64_t>(addr) >> 32)
-
     hsa_amd_aie_ert_start_kernel_data_t * cmd_payload = nullptr;
     if (auto status = hsa_amd_memory_pool_allocate(dev_info.kernarg_memory.memory_pool, 64, 0,
                                                    reinterpret_cast<void **>(&cmd_payload));
@@ -46,34 +43,19 @@ ggml_status ggml_hsa_add(ggml_backend_hsa_context & ctx, ggml_tensor * tensor) {
                        __func__, status);
         return GGML_STATUS_FAILED;
     }
-    cmd_payload->pdi_addr = kernel.pdi_buffer.data; // PDI to use with this command
-    cmd_payload->data[0] = 0x3;                     // Transaction opcode
+    cmd_payload->pdi_addr = kernel.pdi.data; // PDI to use with this command
+    cmd_payload->data[0] = 0x3;              // Transaction opcode
     cmd_payload->data[1] = 0x0;
-    cmd_payload->data[2] = LOW_ADDR(kernel.insts_buffer.data);
-    cmd_payload->data[3] = HIGH_ADDR(kernel.insts_buffer.data);
-    cmd_payload->data[4] = static_cast<std::uint32_t>(kernel.insts_buffer.size);
-    cmd_payload->data[5] = LOW_ADDR(src0->data);
-    cmd_payload->data[6] = HIGH_ADDR(src0->data);
-    cmd_payload->data[7] = LOW_ADDR(src1->data);
-    cmd_payload->data[8] = HIGH_ADDR(src1->data);
-    cmd_payload->data[9] = LOW_ADDR(dst->data);
-    cmd_payload->data[10] = HIGH_ADDR(dst->data);
+    std::tie(cmd_payload->data[3], cmd_payload->data[2]) = ggml_hsa_addr_to_hilo(kernel.insts.data);
+    cmd_payload->data[4] = static_cast<std::uint32_t>(kernel.insts.size);
+    std::tie(cmd_payload->data[6], cmd_payload->data[5]) = ggml_hsa_addr_to_hilo(src0->data);
+    std::tie(cmd_payload->data[8], cmd_payload->data[7]) = ggml_hsa_addr_to_hilo(src1->data);
+    std::tie(cmd_payload->data[10], cmd_payload->data[9]) = ggml_hsa_addr_to_hilo(dst->data);
     cmd_payload->data[11] = element_count * sizeof(std::uint32_t);
     cmd_payload->data[12] = element_count * sizeof(std::uint32_t);
     cmd_payload->data[13] = element_count * sizeof(std::uint32_t);
 
-    std::uint64_t wr_idx = hsa_queue_add_write_index_relaxed(ctx.queue, 1);
-    std::uint64_t packet_id = wr_idx % ctx.queue->size;
-    auto * cmd_pkt = static_cast<hsa_amd_aie_ert_packet_t *>(ctx.queue->base_address) + packet_id;
-    cmd_pkt->state = HSA_AMD_AIE_ERT_STATE_NEW;
-    cmd_pkt->count = 0xC; // # of arguments to put in command
-    cmd_pkt->opcode = HSA_AMD_AIE_ERT_START_CU;
-    cmd_pkt->header.AmdFormat = HSA_AMD_PACKET_TYPE_AIE_ERT;
-    cmd_pkt->header.header = HSA_PACKET_TYPE_VENDOR_SPECIFIC << HSA_PACKET_HEADER_TYPE;
-    cmd_pkt->payload_data = reinterpret_cast<std::uint64_t>(cmd_payload);
-    // TODO add cmd_pkt->completion_signal = ctx.dispatch_signal
-
-    hsa_signal_store_screlease(ctx.queue->doorbell_signal, wr_idx);
+    ggml_hsa_dispatch_patch(ctx, cmd_payload, 12);
 
     return GGML_STATUS_SUCCESS;
 }
