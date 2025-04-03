6ec921317192de1f2eb18f18575dbe6b736f4277
Yiannis Papadopoulos <102817138+ypapadop-amd@users.noreply.github.com>
2025-04-03 12:01:40 -0400

    Various fixes (#38)
    * Renaming pending data functions. Refactor packet dispatch
    
    * Guard all CPU fallback cases
    
    * Internal nodes do not init extra until after graph allocation
    
    * Assert cleanups
    
    * Comments
    
    * Separate CMake support

--- src/ggml-hsa/mul_mat.cpp
+++ src/ggml-hsa/mul_mat.cpp
@@ -11,7 +11,7 @@ ggml_status ggml_hsa_mul_mat(ggml_backend_hsa_context & ctx, ggml_tensor * tenso
     auto & info = ggml_hsa_info();
     auto & dev_info = info.devices[ctx.device];
 
-    GGML_ASSERT(ggml_hsa_supports_mul_mat(dev_info, tensor));
+    assert(ggml_hsa_supports_mul_mat(dev_info, tensor));
 
     auto & tensor_extra = *static_cast<ggml_backend_hsa_tensor_extra *>(tensor->extra);
     const ggml_tensor * src0 = tensor->src[0];
