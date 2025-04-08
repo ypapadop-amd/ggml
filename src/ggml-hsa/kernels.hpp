6fe3563a0fd5e62f6a287b8d4f736eddc3f5baf0
Yiannis Papadopoulos <102817138+ypapadop-amd@users.noreply.github.com>
2025-04-08 17:22:47 -0400

    Stand-alone kernel discovery header (#40)
    * Python script fixes
    
    * Separating kernel discovery to its own header

--- src/ggml-hsa/kernels.hpp
+++ src/ggml-hsa/kernels.hpp
@@ -5,37 +5,6 @@
 #include "ggml-hsa/common.hpp"
 #include "ggml.h"
 
-/**
- * @brief Returns if the kernel exists.
- *
- * @param tensor tensor to load a kernel for
- */
-bool ggml_hsa_kernel_exists(const ggml_hsa_device_info::device_info & dev_info,
-                            const ggml_tensor * tensor);
-
-/**
- * @brief Finds the AIE agent kernel for the tensor's operation.
- *
- * This function will attempt to load the kernel if not found in @ref
- * ggml_backend_hsa_context::aie_kernels. The kernel is cached in a
- * data structure in the `extra` member of @ref ggml_tensor.
- *
- * @param ctx backend context
- * @param tensor tensor to find the kernel for
- * @param kernel kernel for the operation of @p tensor
- */
-ggml_status ggml_hsa_find_aie_kernel(ggml_backend_hsa_context & ctx,
-                                     const ggml_tensor * tensor,
-                                     ggml_hsa_aie_kernel & kernel);
-
-/**
- * @brief Destroys the kernel.
- *
- * @param ctx backend context
- * @param kernel kernel to destroy
- */
-void ggml_hsa_destroy_aie_kernel(ggml_backend_hsa_context & ctx, ggml_hsa_aie_kernel & kernel);
-
 bool ggml_hsa_supports_add(const ggml_hsa_device_info::device_info & dev_info,
                            const ggml_tensor * tensor);
 ggml_status ggml_hsa_add(ggml_backend_hsa_context & ctx, ggml_tensor * tensor);
