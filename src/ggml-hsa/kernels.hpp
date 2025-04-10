e59fb48d5bb30a5506996fe37c812faf5268a47f
Yiannis Papadopoulos <102817138+ypapadop-amd@users.noreply.github.com>
2025-04-10 15:15:53 -0400

    Fix CPU emulation ops (#42)
    * Remove conservative asserts
    
    * Removing cpy kernel. Delegating to the CPU device for supports_op
    
    * Extracting types in example
    
    * Create completely independent fallback graph
    
    * Correct source tensor iteration
    
    * Better messages
    
    * Caching emulated tensors

--- src/ggml-hsa/kernels.hpp
+++ src/ggml-hsa/kernels.hpp
@@ -9,12 +9,6 @@ bool ggml_hsa_supports_add(const ggml_hsa_device_info::device_info & dev_info,
                            const ggml_tensor * tensor);
 ggml_status ggml_hsa_add(ggml_backend_hsa_context & ctx, ggml_tensor * tensor);
 
-#ifdef GGML_HSA_CPU_FALLBACK
-bool ggml_hsa_supports_cpy(const ggml_hsa_device_info::device_info & dev_info,
-                           const ggml_tensor * tensor);
-ggml_status ggml_hsa_cpy(ggml_backend_hsa_context & ctx, ggml_tensor * tensor);
-#endif
-
 bool ggml_hsa_supports_mul_mat(const ggml_hsa_device_info::device_info & dev_info,
                                const ggml_tensor * tensor);
 ggml_status ggml_hsa_mul_mat(ggml_backend_hsa_context & ctx, ggml_tensor * tensor);
