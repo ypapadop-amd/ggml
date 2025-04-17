3e16de610674f055ca6454402ba6f69f5a55adc8
Yiannis Papadopoulos <102817138+ypapadop-amd@users.noreply.github.com>
2025-04-17 14:24:19 -0500

    Miscelaneous cleanups (#43)
    * Moving dependency inside the conditional
    
    * Caching and duplicating original tensor
    
    * Relying on kernel discovery to answer if the op is supported
    
    * Moving output function to common.hpp

--- src/ggml-hsa/kernels.hpp
+++ src/ggml-hsa/kernels.hpp
@@ -5,10 +5,6 @@
 #include "ggml-hsa/common.hpp"
 #include "ggml.h"
 
-bool ggml_hsa_supports_add(const ggml_hsa_device_info::device_info & dev_info,
-                           const ggml_tensor * tensor);
 ggml_status ggml_hsa_add(ggml_backend_hsa_context & ctx, ggml_tensor * tensor);
 
-bool ggml_hsa_supports_mul_mat(const ggml_hsa_device_info::device_info & dev_info,
-                               const ggml_tensor * tensor);
 ggml_status ggml_hsa_mul_mat(ggml_backend_hsa_context & ctx, ggml_tensor * tensor);
