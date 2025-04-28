7af8550b891389ec21b4c7ae0a0ef57c9a3d164a
Yiannis Papadopoulos <102817138+ypapadop-amd@users.noreply.github.com>
2025-04-28 15:01:36 -0500

    Invoking python script via pybind11 (#50)

--- src/ggml-hsa/iron-kernels/compiler.py
+++ src/ggml-hsa/iron-kernels/compiler.py
@@ -1,4 +1,4 @@
-# compile.py -*- Python -*-
+# compiler.py -*- Python -*-
 
 import argparse
 import subprocess
@@ -7,18 +7,27 @@ import sys
 
 
 def compile_mlir(
-    kernel_source: str, kernel_compile_args: str, mlir_filename: str, output_dir: str
+    kernel_source: str,
+    kernel_compile_args: str,
+    mlir_filename: str,
+    output_directory: str,
 ):
     """
-    Creates an MLIR file from kernel_source.
+    Compiles the IRON kernel source to MLIR.
+
+    Parameters:
+        kernel_source (str): Kernel source file.
+        kernel_compile_args (str): Compile arguments.
+        mlir_filename (str): MLIR output file.
+        output_directory (str): Output and working directory.
     """
-    output_path = os.path.join(output_dir, mlir_filename)
+    output_path = os.path.join(output_directory, mlir_filename)
     cmd = [sys.executable, kernel_source] + list(kernel_compile_args.split())
     with open(output_path, "w", encoding="utf-8") as output_file:
         try:
             subprocess.run(
                 cmd,
-                cwd=output_dir,
+                cwd=output_directory,
                 check=True,
                 stdout=output_file,
                 stderr=sys.stderr,
@@ -28,20 +37,21 @@ def compile_mlir(
 
 
 def compile_pdi(
-    mlir_filename: str, pdi_filename: str, insts_filename: str, output_dir: str
+    mlir_filename: str, pdi_filename: str, insts_filename: str, output_directory: str
 ):
     """
-    Compile an MLIR file to instruction and xclbin files using aiecc.py.
+    Compile an MLIR file to PDI and instruction using aiecc.
 
     Parameters:
-        mlir_file (str): MLIR input file.
-        pdi_file (str): PDI output file.
-        insts_file (str): Instructions output file.
+        mlir_filename (str): MLIR input file.
+        pdi_filename (str): PDI output file.
+        insts_filename (str): Instructions output file.
+        output_directory (str): Output and working directory.
     """
 
-    mlir_path = os.path.join(output_dir, mlir_filename)
-    pdi_output_path = os.path.join(output_dir, pdi_filename)
-    insts_output_path = os.path.join(output_dir, insts_filename)
+    mlir_path = os.path.join(output_directory, mlir_filename)
+    pdi_output_path = os.path.join(output_directory, pdi_filename)
+    insts_output_path = os.path.join(output_directory, insts_filename)
     cmd = [
         "aiecc.py",
         "--alloc-scheme=basic-sequential",
@@ -57,7 +67,7 @@ def compile_pdi(
     try:
         subprocess.run(
             cmd,
-            cwd=output_dir,
+            cwd=output_directory,
             check=True,
             stdout=subprocess.DEVNULL,
             stderr=sys.stderr,
@@ -76,9 +86,42 @@ def file_path(string: str):
     return string
 
 
+def compile_kernel(
+    name: str,
+    kernel_source: str,
+    kernel_compile_args: str,
+    output_directory: str,
+):
+    """
+    Compiles the kernel code to PDI and instruction files.
+    """
+
+    mlir_filename = f"{name}.mlir"
+
+    os.makedirs(output_directory, exist_ok=True)
+
+    compile_mlir(
+        kernel_source=kernel_source,
+        kernel_compile_args=kernel_compile_args,
+        mlir_filename=mlir_filename,
+        output_directory=output_directory,
+    )
+
+    compile_pdi(
+        mlir_filename=mlir_filename,
+        pdi_filename=f"{name}.pdi",
+        insts_filename=f"{name}_insts.bin",
+        output_directory=output_directory,
+    )
+
+
 def main():
+    """
+    Main function for use during AOT compilation.
+    """
+
     parser = argparse.ArgumentParser(
-        prog="compile.py",
+        prog="compiler.py",
         description="Compiles IRON kernels",
     )
     parser.add_argument(
@@ -108,27 +151,13 @@ def main():
 
     args = parser.parse_args()
 
-    os.makedirs(args.output_directory, exist_ok=True)
-
-    mlir_filename = f"{args.name}.mlir"
-
-    compile_mlir(
+    compile_kernel(
+        name=args.name,
         kernel_source=args.kernel_source,
         kernel_compile_args=args.kernel_compile_args,
-        mlir_filename=mlir_filename,
-        output_dir=args.output_directory,
-    )
-
-    compile_pdi(
-        mlir_filename=mlir_filename,
-        pdi_filename=f"{args.name}.pdi",
-        insts_filename=f"{args.name}_insts.bin",
-        output_dir=args.output_directory,
+        output_directory=args.output_directory,
     )
 
 
 if __name__ == "__main__":
     main()
-else:
-    print("Not meant to be imported")
-    sys.exit(1)
