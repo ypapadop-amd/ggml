e7ee32e5b28ce2bb941c9052509d2188d6197aed
Yiannis Papadopoulos <102817138+ypapadop-amd@users.noreply.github.com>
2025-04-28 15:13:45 -0500

    Adding copyright (#51)

--- src/ggml-hsa/ggml-hsa.cpp
+++ src/ggml-hsa/ggml-hsa.cpp
@@ -1,3 +1,5 @@
+// Copyright (c) 2024-2025 Advanced Micro Devices, Inc. All Rights Reserved.
+
 #include "ggml-hsa.h"
 #include "ggml-backend-impl.h"
 #include "ggml-impl.h"
--- src/ggml-hsa/iron-kernels/__init__.py
+++ src/ggml-hsa/iron-kernels/__init__.py
@@ -1,3 +1,5 @@
+#  Copyright (c) 2025 Advanced Micro Devices, Inc. All Rights Reserved.
+
 __all__ = ["compile_kernel", "add", "mul_mat"]
 
 from . import compile_kernel
