ddb40cf45262414c9542fda9608e9139061b36ba
Yiannis Papadopoulos <102817138+ypapadop-amd@users.noreply.github.com>
2025-05-16 11:08:04 -0400

    Associating core functions with kernels (#66)
    * Use compiler wrappers from IRON
    
    * Decorator to associate core function compile specifications with kernels
    
    * Refactor

--- src/ggml-hsa/iron_kernels/__init__.py
+++ src/ggml-hsa/iron_kernels/__init__.py
@@ -7,6 +7,7 @@ __all__ = [
     "to_device",
     "str_to_dtype",
     "dtype_to_str",
+    "core_function_compile_spec",
 ]
 
 from .compiler import (
@@ -16,4 +17,5 @@ from .compiler import (
     str_to_dtype,
     dtype_to_str,
     compile_kernel,
+    core_function_compile_spec,
 )
