247e08baf6c9b776c306c228e1f6503c8ba2b811
Yiannis Papadopoulos <102817138+ypapadop-amd@users.noreply.github.com>
2025-05-28 11:55:57 -0400

    Flatten tensors (#71)
    * Simplify kernel name generation
    
    * Stub for gelu erf unary op
    
    * Asserts for non-contiguous tensors
    
    * Flatten tensor op if valid to do so
    
    * Cleanup of JIT compilation process. Adding missing unary op.
    
    * Descriptive log errors

--- src/ggml-hsa/kernel-discovery.cpp
+++ src/ggml-hsa/kernel-discovery.cpp
@@ -59,30 +59,43 @@ static const fs::path cached_kernel_dir = [] {
 }();
 
 /**
+ * @brief Returns if the tensor can be flattened.
+ *
+ * A tensor can be flattened if it participates in an operation that is independent of the tensor's
+ * dimensions.
+ */
+constexpr bool ggml_hsa_tensor_can_flatten(const ggml_tensor * tensor) {
+    return (tensor->op == GGML_OP_UNARY) || (tensor->op == GGML_OP_SQR) ||
+           (tensor->op == GGML_OP_SQRT) || (tensor->op == GGML_OP_LOG) ||
+           (tensor->op == GGML_OP_SIN) || (tensor->op == GGML_OP_COS) ||
+           (tensor->op == GGML_OP_SILU_BACK) || (tensor->op == GGML_OP_LEAKY_RELU);
+}
+
+/**
  * @brief Creates a kernel name for the operation in tensor @p tensor.
  */
 static ggml_status ggml_hsa_create_kernel_name(const ggml_tensor * tensor,
                                                std::string & kernel_name) {
     if ((tensor->op < GGML_OP_NONE) || (tensor->op >= GGML_OP_COUNT)) {
-        GGML_LOG_ERROR("%s: Tensor operation index out of bounds (%d >= GGML_OP_COUNT)\n", __func__,
-                       tensor->op);
+        GGML_LOG_ERROR("%s: Tensor \"%s\" operation index out of bounds (%d >= GGML_OP_COUNT)\n",
+                       __func__, ggml_get_name(tensor), tensor->op);
         return GGML_STATUS_FAILED;
     }
+
+    const bool flatten = ggml_hsa_tensor_can_flatten(tensor);
 
     std::ostringstream oss;
     std::string_view op_name = ggml_op_desc(tensor);
     std::transform(op_name.begin(), op_name.end(), std::ostreambuf_iterator(oss),
                    [&](char c) { return std::tolower(c); });
     oss << '-';
-    ggml_hsa_output_tensor(tensor, oss);
-    oss << '-';
-    ggml_hsa_output_tensor(tensor->src[0], oss);
-    for (int i = 1; i < GGML_MAX_SRC; ++i) {
+    ggml_hsa_output_tensor(tensor, oss, flatten);
+    for (int i = 0; i < GGML_MAX_SRC; ++i) {
         if (tensor->src[i] == nullptr) {
             break;
         }
         oss << '-';
-        ggml_hsa_output_tensor(tensor->src[i], oss);
+        ggml_hsa_output_tensor(tensor->src[i], oss, flatten);
     }
     kernel_name = oss.str();
 
