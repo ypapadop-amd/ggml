e672c970a6552c2361d403236f70079be6ffcc82
Yiannis Papadopoulos <102817138+ypapadop-amd@users.noreply.github.com>
2025-06-05 22:36:08 -0400

    Host implementations for CPY, DUP, CONT (#74)
    * Add operators in backend switch-cases
    
    * Correcting includes
    
    * Adding supported data types to README
    
    * Stubs for dup, cpy, cont
    
    * Adding function to wait for all packets
    
    * Always break after template
    
    * Removing overly restrictive asserts
    
    * Updating documentation
    
    * Naive copy tensor to tensor implementation
    
    * Support datatype conversion
    
    * Adding more cases for tensor copy

--- include/ggml-hsa.h
+++ include/ggml-hsa.h
@@ -11,22 +11,22 @@ extern "C" {
 #define GGML_HSA_MAX_DEVICES 16
 
 // backend API
-GGML_BACKEND_API ggml_backend_t ggml_backend_hsa_init(int device);
+GGML_BACKEND_API ggml_backend_t ggml_backend_hsa_init(int32_t device);
 
 GGML_BACKEND_API bool ggml_backend_is_hsa(ggml_backend_t backend);
 
 // device buffer
-GGML_BACKEND_API ggml_backend_buffer_type_t ggml_backend_hsa_buffer_type(int device);
+GGML_BACKEND_API ggml_backend_buffer_type_t ggml_backend_hsa_buffer_type(int32_t device);
 
 // split tensor buffer that splits matrices by rows across multiple devices
-GGML_BACKEND_API ggml_backend_buffer_type_t ggml_backend_hsa_split_buffer_type(int main_device, const float * tensor_split);
+GGML_BACKEND_API ggml_backend_buffer_type_t ggml_backend_hsa_split_buffer_type(int32_t main_device, const float * tensor_split);
 
 // pinned host buffer for use with the CPU backend for faster copies between CPU and GPU
 GGML_BACKEND_API ggml_backend_buffer_type_t ggml_backend_hsa_host_buffer_type(void);
 
-GGML_BACKEND_API int  ggml_backend_hsa_get_device_count(void);
-GGML_BACKEND_API void ggml_backend_hsa_get_device_description(int device, char * description, size_t description_size);
-GGML_BACKEND_API void ggml_backend_hsa_get_device_memory(int device, size_t * free, size_t * total);
+GGML_BACKEND_API int32_t ggml_backend_hsa_get_device_count(void);
+GGML_BACKEND_API void ggml_backend_hsa_get_device_description(int32_t device, char * description, size_t description_size);
+GGML_BACKEND_API void ggml_backend_hsa_get_device_memory(int32_t device, size_t * free, size_t * total);
 
 GGML_BACKEND_API bool ggml_backend_hsa_register_host_buffer(void * buffer, size_t size);
 GGML_BACKEND_API void ggml_backend_hsa_unregister_host_buffer(void * buffer);
