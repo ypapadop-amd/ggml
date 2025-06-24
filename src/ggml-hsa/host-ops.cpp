0fa0a6feb38ae0db8a7192c38407ce8a62eed636
Yiannis Papadopoulos <102817138+ypapadop-amd@users.noreply.github.com>
2025-06-24 14:37:25 -0400

    Various cleanups (#81)
    * Factoring out ggml_type traits
    
    * Fixing incorrect name
    
    * Kernel support query does not compile kernel
    
    * Flattening property of the tensor. Additional helper functions.
    
    * Library dir is only needed for JIT compilation.
    
    * Refactoring flattening query
    
    * Adding supported data types
    
    * Moving supported types to device_info. Removing deadcode.
    
    * Renaming error checking macros

--- src/ggml-hsa/host-ops.cpp
+++ src/ggml-hsa/host-ops.cpp
@@ -2,63 +2,10 @@
 
 #include "ggml-hsa/host-ops.hpp"
 
-#include "ggml-hsa/common.hpp"
-#include "ggml-impl.h"
-
 #include <cassert>
-#include <cstddef>
 
-/**
- * @brief @ref ggml_typetype traits.
- */
-template <ggml_type T>
-struct ggml_hsa_type_traits;
-
-template <>
-struct ggml_hsa_type_traits<GGML_TYPE_F32> {
-    static constexpr ggml_type ggml_type = GGML_TYPE_F32;
-    using type = float;
-    static constexpr bool is_fundamental = true;
-};
-
-template <>
-struct ggml_hsa_type_traits<GGML_TYPE_F16> {
-    static constexpr ggml_type ggml_type = GGML_TYPE_F16;
-    using type = ggml_fp16_t;
-    static constexpr bool is_fundamental = false;
-    static constexpr auto to_fp32 = [](ggml_fp16_t v) -> float { return GGML_FP16_TO_FP32(v); };
-    static constexpr auto from_fp32 = [](float v) -> ggml_fp16_t { return GGML_FP32_TO_FP16(v); };
-};
-
-template <>
-struct ggml_hsa_type_traits<GGML_TYPE_I8> {
-    static constexpr ggml_type ggml_type = GGML_TYPE_I8;
-    using type = std::int8_t;
-    static constexpr bool is_fundamental = true;
-};
-
-template <>
-struct ggml_hsa_type_traits<GGML_TYPE_I16> {
-    static constexpr ggml_type ggml_type = GGML_TYPE_I16;
-    using type = std::int16_t;
-    static constexpr bool is_fundamental = true;
-};
-
-template <>
-struct ggml_hsa_type_traits<GGML_TYPE_I32> {
-    static constexpr ggml_type ggml_type = GGML_TYPE_I32;
-    using type = std::int32_t;
-    static constexpr bool is_fundamental = true;
-};
-
-template <>
-struct ggml_hsa_type_traits<GGML_TYPE_BF16> {
-    static constexpr ggml_type ggml_type = GGML_TYPE_BF16;
-    using type = ggml_bf16_t;
-    static constexpr bool is_fundamental = false;
-    static constexpr auto to_fp32 = [](ggml_bf16_t v) -> float { return GGML_BF16_TO_FP32(v); };
-    static constexpr auto from_fp32 = [](float v) -> ggml_bf16_t { return GGML_FP32_TO_BF16(v); };
-};
+#include "ggml-hsa/common.hpp"
+#include "ggml-hsa/type-traits.hpp"
 
 /**
  * @brief Copies the data from the source tensor to a destination tensor with the same shape.
