fcedc90aaa878923cb783f08fecf5da3b3fc4df7
Yiannis Papadopoulos <Yiannis.Papadopoulos@amd.com>
2025-06-30 14:38:26 -0400

    Adding stride and if the tensor is contiguous

--- src/ggml-hsa/kernel-compiler.cpp
+++ src/ggml-hsa/kernel-compiler.cpp
@@ -115,39 +115,25 @@ ggml_hsa_get_kernel_jit_info(const ggml_tensor * tensor) {
 }
 
 /**
- * @brief Creates a @p py::tuple from the tensor dimensions.
+ * @brief Creates a @p py::tuple from the tensor shape.
  */
-static py::tuple ggml_hsa_tensor_dims_as_pytuple(const ggml_tensor * tensor) {
-    auto dims_tuple = py::tuple(GGML_MAX_DIMS);
+static py::tuple ggml_hsa_tensor_ne_as_pytuple(const ggml_tensor * tensor) {
+    auto shape = py::tuple(GGML_MAX_DIMS);
     for (auto i = 0; i < GGML_MAX_DIMS; ++i) {
-        dims_tuple[i] = py::int_(tensor->ne[i]);
+        shape[i] = py::int_(tensor->ne[i]);
     }
-    return dims_tuple;
+    return shape;
 }
 
 /**
- * @brief Returns if the tensor is not supported.
+ * @brief Creates a @p py::tuple from the tensor strides.
  */
-static bool ggml_hsa_unsupported_tensor(const ggml_tensor * tensor) {
-    if (ggml_hsa_tensor_can_flatten(tensor)) {
-        return false;
+static py::tuple ggml_hsa_tensor_nb_as_pytuple(const ggml_tensor * tensor) {
+    auto stride = py::tuple(GGML_MAX_DIMS);
+    for (auto i = 0; i < GGML_MAX_DIMS; ++i) {
+        stride[i] = py::int_(tensor->nb[i]);
     }
-
-    // non-contiguous tensors are not yet supported
-    if (!ggml_is_contiguous(tensor)) {
-        return true;
-    }
-    for (int i = 0; i < GGML_MAX_SRC; ++i) {
-        const auto src = tensor->src[i];
-        if (src == nullptr) {
-            break;
-        }
-        if (!ggml_is_contiguous(src)) {
-            return true;
-        }
-    }
-
-    return false;
+    return stride;
 }
 
 ggml_status ggml_hsa_compile_kernel(const ggml_hsa_device_info::device_info & dev_info,
@@ -155,11 +141,6 @@ ggml_status ggml_hsa_compile_kernel(const ggml_hsa_device_info::device_info & de
                                     const std::string & exported_name,
                                     const std::filesystem::path & output_path) {
     using namespace pybind11::literals;
-
-    if (ggml_hsa_unsupported_tensor(tensor)) {
-        GGML_LOG_INFO("%s: Tensor \"%s\" is not supported\n", __func__, ggml_get_name(tensor));
-        return GGML_STATUS_FAILED;
-    }
 
     // retrieve the JIT compilation information for the kernel
     const auto & kernel_jit_info = ggml_hsa_get_kernel_jit_info(tensor);
@@ -191,11 +172,15 @@ ggml_status ggml_hsa_compile_kernel(const ggml_hsa_device_info::device_info & de
         for (auto i = 0; i < src_tensor_count; ++i) {
             const auto src_tensor = tensor->src[i];
             input_tensors[i] =
-                tensor_desc_ctor("shape"_a = ggml_hsa_tensor_dims_as_pytuple(src_tensor),
-                                 "dtype"_a = ggml_type_name(src_tensor->type));
+                tensor_desc_ctor("dtype"_a = ggml_type_name(src_tensor->type),
+                                 "shape"_a = ggml_hsa_tensor_ne_as_pytuple(src_tensor),
+                                 "stride"_a = ggml_hsa_tensor_nb_as_pytuple(src_tensor),
+                                 "contiguous"_a = ggml_is_contiguous(src_tensor));
         }
-        auto output_tensor = tensor_desc_ctor("shape"_a = ggml_hsa_tensor_dims_as_pytuple(tensor),
-                                              "dtype"_a = ggml_type_name(tensor->type));
+        auto output_tensor = tensor_desc_ctor("dtype"_a = ggml_type_name(tensor->type),
+                                              "shape"_a = ggml_hsa_tensor_ne_as_pytuple(tensor),
+                                              "stride"_a = ggml_hsa_tensor_nb_as_pytuple(tensor),
+                                              "contiguous"_a = ggml_is_contiguous(tensor));
 
         // compile the kernel
         auto iron_compiler = py::module_::import("build");
@@ -206,12 +191,12 @@ ggml_status ggml_hsa_compile_kernel(const ggml_hsa_device_info::device_info & de
             "output_tensor"_a = std::move(output_tensor), "exported_name"_a = exported_name,
             "output_directory"_a = output_directory.string(), "verbose"_a = verbose_compilation);
     } catch (const pybind11::error_already_set & ex) {
-        GGML_LOG_ERROR("%s: JIT compilation for kernel %s failed:\n%s\n", __func__,
+        GGML_LOG_ERROR("%s: compilation for kernel %s failed:\n%s\n", __func__,
                        exported_name.c_str(), ex.what());
         return GGML_STATUS_FAILED;
     }
 
-    GGML_LOG_INFO("%s: Generated kernel %s in %s\n", __func__, exported_name.c_str(),
+    GGML_LOG_INFO("%s: generated kernel %s in %s\n", __func__, exported_name.c_str(),
                   output_directory.c_str());
 
     return GGML_STATUS_SUCCESS;
