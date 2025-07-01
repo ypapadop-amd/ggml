ee7929ad59ec206453e02235869ab3d916278996
Yiannis Papadopoulos <102817138+ypapadop-amd@users.noreply.github.com>
2025-07-01 12:05:27 -0400

    Flatten view support (#85)
    * Support for views on unary ops
    
    * Support views on flattened tensors
    
    * Reorganizing JIT files
    
    * Only flatten when having contiguous tensors
    
    * Updating comments
    
    * Additional comments
    
    * Adding logging and more traits support
    
    * Adding __init__ and extra run-time checks
    
    * Updating pybind version
    
    * Adding stride and if the tensor is contiguous
    
    * Simplifying tensor naming
    
    * Using pybind11 from IRON
    
    * Renaming variable to avoid name clash with gcc
    
    * Initializing python interpreter once to avoid python library reinit issues
    
    * Return a new aie.iron.device object every time
    
    * Fix comments
    
    * Update mul_mat

---
+++ src/ggml-hsa/iron/utils/__init__.py
@@ -0,0 +1,24 @@
+# __init__.py -*- Python -*-
+# (c) Copyright 2025 Advanced Micro Devices, Inc. or its affiliates
+
+__all__ = [
+    "CoreFunctionInfo",
+    "core_function",
+    "dtype_to_str",
+    "TensorDesc",
+    "tensordesc",
+    "compile_kernel",
+]
+
+from .build import compile_kernel
+
+from .core_function import (
+    CoreFunctionInfo,
+    core_function,
+)
+
+from .tensor_desc import (
+    dtype_to_str,
+    TensorDesc,
+    tensordesc,
+)
