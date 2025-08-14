110e30250bf3a566c1036f99320bbc819a7e42bc
Yiannis Papadopoulos <102817138+ypapadop-amd@users.noreply.github.com>
2025-08-14 16:49:06 -0400

    Kernel compilation at tensor initialization (#98)
    * Managing memory using unique_ptr
    
    * Moving kernel compilation to tensor init. Removing backend context where possible.
    
    * Generalizing ggml_hsa_get_device_info()
    
    * Using uniform init
    
    * Formatting
    
    * Memory management with smart pointers and kernel caching at device level.
    
    * Moving tensor name creation out of discovery
    
    * Replacing tensor pointers with references
    
    * Creating kernel name at tensor init

--- src/ggml-hsa/kernel-discovery.hpp
+++ src/ggml-hsa/kernel-discovery.hpp
@@ -5,39 +5,23 @@
 #include "ggml-hsa/common.hpp"
 #include "ggml.h"
 
-/**
- * @brief Returns if the kernel is supported for the device and tensor.
- *
- * @note This function may trigger kernel compilation if JIT is enabled.
- *
- * @param dev_info[in] device information
- * @param tensor[in] tensor to load a kernel for
- */
-bool ggml_hsa_kernel_is_supported(const ggml_hsa_device_info::device_info & dev_info,
-                                  const ggml_tensor * tensor);
+#include <string>
 
 /**
  * @brief Creates the AIE kernel for the tensor's operation.
  *
  * This function will try the following until one succeeds:
- *   -# attempt to load the kernel from the @ref ctx kernel cache in @ref
- *      ggml_backend_hsa_context::aie_kernels,
- *   -# load the kernel from disk from the system kernel directory,
- *   -# load the kernel from disk from the user kernel directory,
- *   -# JIT compile the kernel and store it to the user kernel directory.
- * If nothing works, an error message will be returned.
+ *   -# load the kernel from the precompiled kernel directory,
+ *   -# load the kernel from the cached kernel directory,
+ *   -# JIT compile the kernel and store it to the cached kernel directory.
+ * If none of the above succeeds, an error message will be returned.
  *
- * @param[in] ctx backend context
+ * @param[in] dev_info device information
+ * @param[in] kernel_name kernel name
  * @param[in] tensor tensor to find the kernel for
  * @param[out] kernel kernel for the operation of @p tensor
  */
-ggml_status ggml_hsa_create_aie_kernel(ggml_backend_hsa_context & ctx,
-                                       const ggml_tensor * tensor,
+ggml_status ggml_hsa_create_aie_kernel(const ggml_hsa_device_info::device_info & dev_info,
+                                       const std::string & kernel_name,
+                                       const ggml_tensor & tensor,
                                        ggml_hsa_aie_kernel & kernel);
-
-/**
- * @brief Destroys the kernel.
- *
- * @param[in] kernel kernel to destroy
- */
-void ggml_hsa_destroy_aie_kernel(ggml_hsa_aie_kernel & kernel);
