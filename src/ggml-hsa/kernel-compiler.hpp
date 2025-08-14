e9c0ce472a86b8532e213211d8f2d2242b194ec5
Yiannis Papadopoulos <102817138+ypapadop-amd@users.noreply.github.com>
2025-08-14 16:49:06 -0400

    Kernel compilation at tensor initialization (#98)
    * Managing memory using unique_ptr
    
    * Moving kernel compilation to tensor init. Removing backend context where possible.
    
    * Generalizing ggml_hsa_get_device_info()
    
    * Using uniform init
    
    * Formatting
    
    * Memory management with smart pointers and kernel caching at device level.
    
    * Moving tensor name creation out of discovery
    
    * Replacing tensor pointers with references
    
    * Creating kernel name at tensor init

--- src/ggml-hsa/kernel-compiler.hpp
+++ src/ggml-hsa/kernel-compiler.hpp
@@ -16,6 +16,6 @@
  * @param[in] output_path directory to write kernel to
  */
 ggml_status ggml_hsa_compile_kernel(const ggml_hsa_device_info::device_info & dev_info,
-                                    const ggml_tensor * tensor,
+                                    const ggml_tensor & tensor,
                                     const std::string & kernel_name,
                                     const std::filesystem::path & output_path);
