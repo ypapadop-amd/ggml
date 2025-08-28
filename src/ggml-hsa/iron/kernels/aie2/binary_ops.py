6625f56944142339e0bcffe5f0c0599bfd5155ed
Yiannis Papadopoulos <102817138+ypapadop-amd@users.noreply.github.com>
2025-08-28 14:05:02 -0400

    Kernel refactor (#102)
    * Relaxing checks in unary_ops
    
    * Using same arg names between unary and binary ops high-order functions
    
    * Relaxing checks in binary_ops
    
    * Comment formatting

--- src/ggml-hsa/iron/kernels/aie2/binary_ops.py
+++ src/ggml-hsa/iron/kernels/aie2/binary_ops.py
@@ -18,7 +18,16 @@ from utils import arch_to_device
 
 
 def binary_op(device, input_tensor0, input_tensor1, op, output):
-    """Implements output = input_tensor0 op input_tensor1"""
+    """
+    Implements output = input_tensor0 op input_tensor1
+
+    Parameters:
+        device (str): Target device.
+        input_tensor0: First input tensor.
+        input_tensor1: Second input tensor.
+        op: Binary operator.
+        output: Output tensor.
+    """
 
     if (
         not input_tensor0.contiguous
@@ -37,7 +46,11 @@ def binary_op(device, input_tensor0, input_tensor1, op, output):
             f"Incompatible input and output shapes ({input_tensor0.shape} != {output.shape})."
         )
 
-    if input_tensor0.shape[2] != 1 or input_tensor0.shape[3] != 1:
+    if (
+        input_tensor0.shape[2] != 1
+        or input_tensor0.shape[2] != 1
+        or input_tensor0.shape[3] != 1
+    ):
         raise ValueError(f"Unsupported shape ({input_tensor0.shape}).")
 
     num_elements = np.size(input_tensor0)
@@ -48,47 +61,39 @@ def binary_op(device, input_tensor0, input_tensor1, op, output):
         )
     num_elements_div_n = num_elements // n
 
-    if input_tensor0.dtype != input_tensor1.dtype:
-        raise ValueError(
-            f"Incompatible input data types ({input_tensor0.dtype} != {input_tensor1.dtype})."
-        )
-    if input_tensor0.dtype != output.dtype:
-        raise ValueError(
-            f"Incompatible output data types ({input_tensor0.dtype} != {output.dtype})."
-        )
-    dtype = input_tensor0.dtype
-
-    # Define tensor types
-    tensor_ty = np.ndarray[(num_elements,), np.dtype[dtype]]
-    tile_ty = np.ndarray[(n,), np.dtype[dtype]]
-
     # AIE-array data movement with object fifos
-    of_in1 = ObjectFifo(tile_ty, name="in1")
-    of_in2 = ObjectFifo(tile_ty, name="in2")
-    of_out = ObjectFifo(tile_ty, name="out")
+    input0_tile_ty = np.ndarray[(n,), np.dtype[input_tensor0.dtype]]
+    input1_tile_ty = np.ndarray[(n,), np.dtype[input_tensor1.dtype]]
+    output_tile_ty = np.ndarray[(n,), np.dtype[output.dtype]]
+    of_in0 = ObjectFifo(input0_tile_ty, name="in1")
+    of_in1 = ObjectFifo(input1_tile_ty, name="in2")
+    of_out = ObjectFifo(output_tile_ty, name="out")
 
     # Define a task that will run on a compute tile
-    def core_body(of_in1, of_in2, of_out):
+    def core_body(of_in0, of_in1, of_out):
         # Number of sub-vector "tile" iterations
         for _ in range_(num_elements_div_n):
+            elem_in0 = of_in0.acquire(1)
             elem_in1 = of_in1.acquire(1)
-            elem_in2 = of_in2.acquire(1)
             elem_out = of_out.acquire(1)
             for i in range_(n):
-                elem_out[i] = op(elem_in1[i], elem_in2[i])
+                elem_out[i] = op(elem_in0[i], elem_in1[i])
+            of_in0.release(1)
             of_in1.release(1)
-            of_in2.release(1)
             of_out.release(1)
 
     # Create a worker to run the task on a compute tile
-    worker = Worker(core_body, fn_args=[of_in1.cons(), of_in2.cons(), of_out.prod()])
+    worker = Worker(core_body, fn_args=[of_in0.cons(), of_in1.cons(), of_out.prod()])
 
     # Runtime operations to move data to/from the AIE-array
+    input0_tensor_ty = np.ndarray[(num_elements,), np.dtype[input_tensor0.dtype]]
+    input1_tensor_ty = np.ndarray[(num_elements,), np.dtype[input_tensor1.dtype]]
+    output_tensor_ty = np.ndarray[(num_elements,), np.dtype[output.dtype]]
     rt = Runtime()
-    with rt.sequence(tensor_ty, tensor_ty, tensor_ty) as (A, B, C):
+    with rt.sequence(input0_tensor_ty, input1_tensor_ty, output_tensor_ty) as (A, B, C):
         rt.start(worker)
-        rt.fill(of_in1.prod(), A)
-        rt.fill(of_in2.prod(), B)
+        rt.fill(of_in0.prod(), A)
+        rt.fill(of_in1.prod(), B)
         rt.drain(of_out.cons(), C, wait=True)
 
     # Place program components (assign them resources on the device) and generate an MLIR module
