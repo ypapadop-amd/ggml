9e993d07fc5cbc26a83b9fc248da5ab0d9a3be22
Yiannis Papadopoulos <102817138+ypapadop-amd@users.noreply.github.com>
2025-09-03 09:34:00 -0400

    Updated environment instructions (#106)

--- src/ggml-hsa/env_setup.sh
+++ src/ggml-hsa/env_setup.sh
@@ -2,14 +2,10 @@
 # Copyright (c) 2025 Advanced Micro Devices, Inc. All Rights Reserved.
 
 SCRIPT_DIR_NAME=$(dirname -- "${BASH_SOURCE[0]}")
+VENV_NAME=ggml_hsa_env
 
-python3 -m venv ggml_hsa_env
-source ggml_hsa_env/bin/activate
+python3 -m venv ${VENV_NAME}
+source ${VENV_NAME}/bin/activate
 python3 -m pip install --upgrade pip
 python3 -m pip install -r ${SCRIPT_DIR_NAME}/requirements.txt
 HOST_MLIR_PYTHON_PACKAGE_PREFIX=aie python3 -m pip install -r ${SCRIPT_DIR_NAME}/requirements_extras.txt
-
-MLIR_AIE_INSTALL_DIR="$(pip show mlir_aie | grep ^Location: | awk '{print $2}')/mlir_aie"
-
-export PATH=${MLIR_AIE_INSTALL_DIR}/bin:${PATH}
-export PYTHONPATH=${MLIR_AIE_INSTALL_DIR}/python:${PYTHONPATH}
