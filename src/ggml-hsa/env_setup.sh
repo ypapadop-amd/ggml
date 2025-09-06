0d32ec4ee6bc556f6d4bc221490d8158e4fa0374
Yiannis Papadopoulos <Yiannis.Papadopoulos@amd.com>
2025-09-05 21:15:15 -0400

    Updating IRON environment set-up

--- src/ggml-hsa/env_setup.sh
+++ src/ggml-hsa/env_setup.sh
@@ -7,5 +7,5 @@ VENV_NAME=ggml_hsa_env
 python3 -m venv ${VENV_NAME}
 source ${VENV_NAME}/bin/activate
 python3 -m pip install --upgrade pip
+MLIR_PYTHON_EXTRAS_SET_VERSION="0.0.8.3" HOST_MLIR_PYTHON_PACKAGE_PREFIX="aie" \
 python3 -m pip install -r ${SCRIPT_DIR_NAME}/requirements.txt
-HOST_MLIR_PYTHON_PACKAGE_PREFIX=aie python3 -m pip install -r ${SCRIPT_DIR_NAME}/requirements_extras.txt
