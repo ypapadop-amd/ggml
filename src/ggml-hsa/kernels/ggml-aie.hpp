885b4bee1627242b82ea356417e4922de199fad5
Yiannis Papadopoulos <102817138+ypapadop-amd@users.noreply.github.com>
2025-10-23 16:21:20 -0400

    Adding new unary ops (#141)
    * Adding new unary ops
    
    * Assert if type is not floating point
    
    * Fix floor implementation

--- src/ggml-hsa/kernels/ggml-aie.hpp
+++ src/ggml-hsa/kernels/ggml-aie.hpp
@@ -3,6 +3,8 @@
 #pragma once
 
 #include <cstdint>
+#include <type_traits>
+
 #include <aie_api/aie.hpp>
 
 using i8 = std::int8_t;
@@ -10,3 +12,11 @@ using i16 = std::int16_t;
 using i32 = std::int32_t;
 using bf16 = bfloat16;
 using f32 = float;
+
+template <typename T>
+struct is_floating_point
+    : public std::integral_constant<bool,
+                                    std::is_floating_point_v<T> || std::is_same_v<T, bfloat16>> {};
+
+template <typename T>
+constexpr bool is_floating_point_v = is_floating_point<T>::value;
