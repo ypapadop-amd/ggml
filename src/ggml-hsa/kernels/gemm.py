d98679191a16ccaa31bd34df914e33b9dddf823f
Yiannis Papadopoulos <102817138+ypapadop-amd@users.noreply.github.com>
2025-11-17 16:35:46 -0500

    aie2p MAT_MUL (#150)

--- src/ggml-hsa/kernels/gemm.py
+++ src/ggml-hsa/kernels/gemm.py
@@ -6,6 +6,7 @@
 # (c) Copyright 2025 AMD Inc.
 
 import argparse
+from os import path
 import numpy as np
 
 from aie.extras.context import mlir_mod_ctx
@@ -15,7 +16,7 @@ from aie.dialects.aiex import *
 from aie.helpers.dialects.ext.scf import _for as range_
 from aie.helpers.taplib import TensorAccessPattern, TensorAccessSequence
 
-from aie.iron import str_to_dtype
+from aie.iron import dtype_to_str, str_to_dtype, ExternalFunction
 
 
 microkernel_mac_dim_map = {
@@ -630,3 +631,156 @@ def my_matmul(
 
 if __name__ == "__main__":
     main()
+
+
+def create_mat_mul_external_functions(
+    arch: str,
+    input_tensors: list,
+    output_tensor,
+):
+    """
+    Returns the parameters and names of the external functions for matrix multiplication.
+
+    Args:
+        arch (str): Target architecture.
+        input_tensors: List of two input tensors.
+        output_tensor: Output tensor.
+
+    Returns:
+        A tuple containing:
+            - m: The block size in the M dimension.
+            - n: The block size in the N dimension.
+            - k: The block size in the K dimension.
+            - use_scalar: Boolean indicating if scalar multiplication is used.
+            - mm_fn: The name of the matrix multiplication function.
+            - zero_fn: The name of the zeroing function.
+    """
+    use_scalar = False
+    scalar_suffix = "_scalar" if use_scalar else ""
+
+    num_cols = None
+    if arch == "aie2":
+        num_cols = 4
+        m = 8
+        n = 8
+        k = 8
+    elif arch == "aie2p":
+        num_cols = 8
+        m = 16
+        n = 16
+        k = 16
+    else:
+        raise ValueError(f"Unsupported architecture: {arch}")
+
+    current_dir = path.dirname(path.realpath(__file__))
+    source_file = path.join(current_dir, arch, "mm.cc")
+    compile_args = [
+        f"-DDIM_M={m}",
+        f"-DDIM_N={n}",
+        f"-DDIM_K={k}",
+        f"-D{dtype_to_str(input_tensors[0].dtype)}_{dtype_to_str(output_tensor.dtype)}_ONLY",
+        "-DB_COL_MAJ",
+        "-DC_COL_MAJ",
+    ]
+    object_file_name = "matmul_core_functions.o"
+
+    zero_fn = ExternalFunction(
+        name=f"zero{scalar_suffix}_{dtype_to_str(output_tensor.dtype)}",
+        object_file_name=object_file_name,
+        source_file=source_file,
+        arg_types=[np.ndarray[(m, n), np.dtype[output_tensor.dtype]]],
+        compile_flags=compile_args,
+    )
+
+    matmul_fn = ExternalFunction(
+        name=f"matmul{scalar_suffix}_{dtype_to_str(input_tensors[0].dtype)}_{dtype_to_str(output_tensor.dtype)}",
+        object_file_name=object_file_name,
+        source_file=source_file,
+        arg_types=[
+            np.ndarray[(m, k), np.dtype[input_tensors[0].dtype]],
+            np.ndarray[(k, n), np.dtype[input_tensors[0].dtype]],
+            np.ndarray[(m, n), np.dtype[output_tensor.dtype]],
+        ],
+        compile_flags=compile_args,
+    )
+
+    return (
+        m,
+        n,
+        k,
+        use_scalar,
+        num_cols,
+        zero_fn,
+        matmul_fn,
+    )
+
+
+def ggml_op_mul_mat(
+    arch: str, input_tensors: list, output_tensor, op_params: bytearray
+):
+    """
+    Performs matrix multiplication for GGML using the specified architecture.
+
+    Args:
+        arch (str): Target architecture (e.g., "aie2", "aie2p").
+        input_tensors (list): List of two input tensors (A and B).
+        output_tensor: Output tensor (C).
+        op_params (bytearray): Operation-specific parameters as a bytearray.
+
+    Returns:
+        The MLIR module representing the matrix multiplication operation.
+    """
+    if len(input_tensors) != 2:
+        raise ValueError("Requires two input tensors")
+
+    A = input_tensors[0]  # MxK = A.shape(1) x A.shape(0)
+    B = input_tensors[1]  # KxN = B.shape(0) x B.shape(1)
+    C = output_tensor  # MxN = C.shape(0) x C.shape(1)
+
+    if not A.contiguous or not B.contiguous or not C.contiguous:
+        raise ValueError("Tensors must be contiguous")
+
+    if A.shape[1] != C.shape[0]:
+        raise ValueError(f"Incompatible M for A and C: {A.shape[1]} != {C.shape[0]}")
+
+    if B.shape[1] != C.shape[1]:
+        raise ValueError(f"Incompatible N for B and C: {B.shape[1]} != {C.shape[1]}")
+
+    if A.shape[0] != B.shape[0]:
+        raise ValueError(f"Incompatible K for A and B: {A.shape[0]} != {B.shape[0]}")
+
+    if arch == "aie2":
+        dev = "npu"
+    elif arch == "aie2p":
+        dev = "npu2"
+    else:
+        raise ValueError(f"Unsupported architecture: {arch}")
+
+    m, n, k, use_scalar, num_cols, zero_fn, matmul_fn = (
+        create_mat_mul_external_functions(
+            arch=arch, input_tensors=input_tensors, output_tensor=output_tensor
+        )
+    )
+
+    with mlir_mod_ctx() as ctx:
+        my_matmul(
+            dev=dev,
+            M=A.shape[1],
+            N=B.shape[1],
+            K=A.shape[0],
+            m=m,
+            n=n,
+            k=k,
+            n_aie_cols=num_cols,
+            dtype_in_str=dtype_to_str(A.dtype),
+            dtype_out_str=dtype_to_str(C.dtype),
+            b_col_maj=True,
+            c_col_maj=True,
+            use_scalar=use_scalar,
+            emulate_bf16_mmul_with_bfp16=False,
+            trace_size=0,
+            zero_fn=zero_fn._name,
+            matmul_fn=matmul_fn._name,
+            object_file=matmul_fn.bin_name,
+        )
+        return ctx.module
