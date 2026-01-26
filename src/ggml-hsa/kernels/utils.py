2d7693e4aa6747137ae988dbbfef528fd540f76f
Yiannis Papadopoulos <Yiannis.Papadopoulos@amd.com>
2026-01-26 16:23:50 -0500

    Remove duplicated code

--- src/ggml-hsa/kernels/utils.py
+++ src/ggml-hsa/kernels/utils.py
@@ -1,19 +1,9 @@
-def suppress_import_pyxrt_msg():
-    """Function to suppress pyxrt not found message."""
+# suppress stderr from aie imports until https://github.com/Xilinx/mlir-aie/issues/2833
+# is resolved
 
-    # suppress stderr from aie imports until https://github.com/Xilinx/mlir-aie/issues/2833
-    # is resolved
-    import os
-    import contextlib
-
-    with open(os.devnull, "w", encoding="utf-8") as devnull:
-        with contextlib.redirect_stderr(devnull):
-            import aie.utils
 import os
 import contextlib
 
-# suppress stderr from aie imports until https://github.com/Xilinx/mlir-aie/issues/2833
-# is resolved
 with open(os.devnull, "w", encoding="utf-8") as _ggml_hsa_devnull:
     with contextlib.redirect_stderr(_ggml_hsa_devnull):
         import aie.utils as _ggml_hsa_aie_utils
