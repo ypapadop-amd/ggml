73a0692be2e696c17b9507a1040e77d59106122d
Yiannis Papadopoulos <Yiannis.Papadopoulos@amd.com>
2026-03-09 10:34:58 -0400

    refactor(hsa): rename broadcast op params to use prefix naming convention

--- src/ggml-hsa/kernels/iron/binary_ops.cc
+++ src/ggml-hsa/kernels/iron/binary_ops.cc
@@ -68,40 +68,35 @@ void ggml_op_add_broadcast(const INPUT0_DTYPE * __restrict in0,
                            OUTPUT_DTYPE * __restrict out,
                            int32_t tile_size,
                            int32_t tile_idx,
-                           int32_t ne0_src1,
-                           int32_t ne1_src1,
-                           int32_t ne2_src1,
-                           int32_t ne3_src1,
-                           int32_t ne0_dst,
-                           int32_t ne1_dst,
-                           int32_t ne2_dst) {
+                           int32_t src1_ne0, int32_t src1_ne1, int32_t src1_ne2, int32_t src1_ne3,
+                           int32_t dst_ne0, int32_t dst_ne1, int32_t dst_ne2) {
     event0();
 
     int32_t global_offset = tile_idx * tile_size;
 
     // src1 strides (contiguous layout)
-    int32_t s1 = ne0_src1;
-    int32_t s2 = ne0_src1 * ne1_src1;
-    int32_t s3 = ne0_src1 * ne1_src1 * ne2_src1;
+    int32_t s1 = src1_ne0;
+    int32_t s2 = src1_ne0 * src1_ne1;
+    int32_t s3 = src1_ne0 * src1_ne1 * src1_ne2;
 
     // dst strides for coordinate decomposition
-    int32_t d1 = ne0_dst;
-    int32_t d2 = ne0_dst * ne1_dst;
+    int32_t d1 = dst_ne0;
+    int32_t d2 = dst_ne0 * dst_ne1;
 
     for (int32_t i = 0; i < tile_size; ++i) {
         int32_t g = global_offset + i;
 
         // Decompose into 4D dst coordinates
-        int32_t i0 = g % ne0_dst;
-        int32_t i1 = (g / d1) % ne1_dst;
-        int32_t i2 = (g / d2) % ne2_dst;
-        int32_t i3 = g / (d2 * ne2_dst);
+        int32_t i0 = g % dst_ne0;
+        int32_t i1 = (g / d1) % dst_ne1;
+        int32_t i2 = (g / d2) % dst_ne2;
+        int32_t i3 = g / (d2 * dst_ne2);
 
         // Apply broadcast modulo
-        int32_t j0 = i0 % ne0_src1;
-        int32_t j1 = i1 % ne1_src1;
-        int32_t j2 = i2 % ne2_src1;
-        int32_t j3 = i3 % ne3_src1;
+        int32_t j0 = i0 % src1_ne0;
+        int32_t j1 = i1 % src1_ne1;
+        int32_t j2 = i2 % src1_ne2;
+        int32_t j3 = i3 % src1_ne3;
 
         // src1 index
         int32_t idx_src1 = j0 + j1 * s1 + j2 * s2 + j3 * s3;
@@ -123,40 +118,35 @@ void ggml_op_sub_broadcast(const INPUT0_DTYPE * __restrict in0,
                            OUTPUT_DTYPE * __restrict out,
                            int32_t tile_size,
                            int32_t tile_idx,
-                           int32_t ne0_src1,
-                           int32_t ne1_src1,
-                           int32_t ne2_src1,
-                           int32_t ne3_src1,
-                           int32_t ne0_dst,
-                           int32_t ne1_dst,
-                           int32_t ne2_dst) {
+                           int32_t src1_ne0, int32_t src1_ne1, int32_t src1_ne2, int32_t src1_ne3,
+                           int32_t dst_ne0, int32_t dst_ne1, int32_t dst_ne2) {
     event0();
 
     int32_t global_offset = tile_idx * tile_size;
 
     // src1 strides (contiguous layout)
-    int32_t s1 = ne0_src1;
-    int32_t s2 = ne0_src1 * ne1_src1;
-    int32_t s3 = ne0_src1 * ne1_src1 * ne2_src1;
+    int32_t s1 = src1_ne0;
+    int32_t s2 = src1_ne0 * src1_ne1;
+    int32_t s3 = src1_ne0 * src1_ne1 * src1_ne2;
 
     // dst strides for coordinate decomposition
-    int32_t d1 = ne0_dst;
-    int32_t d2 = ne0_dst * ne1_dst;
+    int32_t d1 = dst_ne0;
+    int32_t d2 = dst_ne0 * dst_ne1;
 
     for (int32_t i = 0; i < tile_size; ++i) {
         int32_t g = global_offset + i;
 
         // Decompose into 4D dst coordinates
-        int32_t i0 = g % ne0_dst;
-        int32_t i1 = (g / d1) % ne1_dst;
-        int32_t i2 = (g / d2) % ne2_dst;
-        int32_t i3 = g / (d2 * ne2_dst);
+        int32_t i0 = g % dst_ne0;
+        int32_t i1 = (g / d1) % dst_ne1;
+        int32_t i2 = (g / d2) % dst_ne2;
+        int32_t i3 = g / (d2 * dst_ne2);
 
         // Apply broadcast modulo
-        int32_t j0 = i0 % ne0_src1;
-        int32_t j1 = i1 % ne1_src1;
-        int32_t j2 = i2 % ne2_src1;
-        int32_t j3 = i3 % ne3_src1;
+        int32_t j0 = i0 % src1_ne0;
+        int32_t j1 = i1 % src1_ne1;
+        int32_t j2 = i2 % src1_ne2;
+        int32_t j3 = i3 % src1_ne3;
 
         // src1 index
         int32_t idx_src1 = j0 + j1 * s1 + j2 * s2 + j3 * s3;
@@ -178,40 +168,35 @@ void ggml_op_mul_broadcast(const INPUT0_DTYPE * __restrict in0,
                            OUTPUT_DTYPE * __restrict out,
                            int32_t tile_size,
                            int32_t tile_idx,
-                           int32_t ne0_src1,
-                           int32_t ne1_src1,
-                           int32_t ne2_src1,
-                           int32_t ne3_src1,
-                           int32_t ne0_dst,
-                           int32_t ne1_dst,
-                           int32_t ne2_dst) {
+                           int32_t src1_ne0, int32_t src1_ne1, int32_t src1_ne2, int32_t src1_ne3,
+                           int32_t dst_ne0, int32_t dst_ne1, int32_t dst_ne2) {
     event0();
 
     int32_t global_offset = tile_idx * tile_size;
 
     // src1 strides (contiguous layout)
-    int32_t s1 = ne0_src1;
-    int32_t s2 = ne0_src1 * ne1_src1;
-    int32_t s3 = ne0_src1 * ne1_src1 * ne2_src1;
+    int32_t s1 = src1_ne0;
+    int32_t s2 = src1_ne0 * src1_ne1;
+    int32_t s3 = src1_ne0 * src1_ne1 * src1_ne2;
 
     // dst strides for coordinate decomposition
-    int32_t d1 = ne0_dst;
-    int32_t d2 = ne0_dst * ne1_dst;
+    int32_t d1 = dst_ne0;
+    int32_t d2 = dst_ne0 * dst_ne1;
 
     for (int32_t i = 0; i < tile_size; ++i) {
         int32_t g = global_offset + i;
 
         // Decompose into 4D dst coordinates
-        int32_t i0 = g % ne0_dst;
-        int32_t i1 = (g / d1) % ne1_dst;
-        int32_t i2 = (g / d2) % ne2_dst;
-        int32_t i3 = g / (d2 * ne2_dst);
+        int32_t i0 = g % dst_ne0;
+        int32_t i1 = (g / d1) % dst_ne1;
+        int32_t i2 = (g / d2) % dst_ne2;
+        int32_t i3 = g / (d2 * dst_ne2);
 
         // Apply broadcast modulo
-        int32_t j0 = i0 % ne0_src1;
-        int32_t j1 = i1 % ne1_src1;
-        int32_t j2 = i2 % ne2_src1;
-        int32_t j3 = i3 % ne3_src1;
+        int32_t j0 = i0 % src1_ne0;
+        int32_t j1 = i1 % src1_ne1;
+        int32_t j2 = i2 % src1_ne2;
+        int32_t j3 = i3 % src1_ne3;
 
         // src1 index
         int32_t idx_src1 = j0 + j1 * s1 + j2 * s2 + j3 * s3;
@@ -233,40 +218,35 @@ void ggml_op_div_broadcast(const INPUT0_DTYPE * __restrict in0,
                            OUTPUT_DTYPE * __restrict out,
                            int32_t tile_size,
                            int32_t tile_idx,
-                           int32_t ne0_src1,
-                           int32_t ne1_src1,
-                           int32_t ne2_src1,
-                           int32_t ne3_src1,
-                           int32_t ne0_dst,
-                           int32_t ne1_dst,
-                           int32_t ne2_dst) {
+                           int32_t src1_ne0, int32_t src1_ne1, int32_t src1_ne2, int32_t src1_ne3,
+                           int32_t dst_ne0, int32_t dst_ne1, int32_t dst_ne2) {
     event0();
 
     int32_t global_offset = tile_idx * tile_size;
 
     // src1 strides (contiguous layout)
-    int32_t s1 = ne0_src1;
-    int32_t s2 = ne0_src1 * ne1_src1;
-    int32_t s3 = ne0_src1 * ne1_src1 * ne2_src1;
+    int32_t s1 = src1_ne0;
+    int32_t s2 = src1_ne0 * src1_ne1;
+    int32_t s3 = src1_ne0 * src1_ne1 * src1_ne2;
 
     // dst strides for coordinate decomposition
-    int32_t d1 = ne0_dst;
-    int32_t d2 = ne0_dst * ne1_dst;
+    int32_t d1 = dst_ne0;
+    int32_t d2 = dst_ne0 * dst_ne1;
 
     for (int32_t i = 0; i < tile_size; ++i) {
         int32_t g = global_offset + i;
 
         // Decompose into 4D dst coordinates
-        int32_t i0 = g % ne0_dst;
-        int32_t i1 = (g / d1) % ne1_dst;
-        int32_t i2 = (g / d2) % ne2_dst;
-        int32_t i3 = g / (d2 * ne2_dst);
+        int32_t i0 = g % dst_ne0;
+        int32_t i1 = (g / d1) % dst_ne1;
+        int32_t i2 = (g / d2) % dst_ne2;
+        int32_t i3 = g / (d2 * dst_ne2);
 
         // Apply broadcast modulo
-        int32_t j0 = i0 % ne0_src1;
-        int32_t j1 = i1 % ne1_src1;
-        int32_t j2 = i2 % ne2_src1;
-        int32_t j3 = i3 % ne3_src1;
+        int32_t j0 = i0 % src1_ne0;
+        int32_t j1 = i1 % src1_ne1;
+        int32_t j2 = i2 % src1_ne2;
+        int32_t j3 = i3 % src1_ne3;
 
         // src1 index
         int32_t idx_src1 = j0 + j1 * s1 + j2 * s2 + j3 * s3;
