18725e86510a540377e416bf68d8f235d90db75c
Yiannis Papadopoulos <Yiannis.Papadopoulos@amd.com>
2026-03-09 09:19:54 -0400

    feat(ggml-hsa): add broadcast support for binary ops in IRON kernels

--- src/ggml-hsa/kernels/iron/binary_ops.cc
+++ src/ggml-hsa/kernels/iron/binary_ops.cc
@@ -61,4 +61,117 @@ void ggml_op_div(const INPUT0_DTYPE * __restrict in0,
 
 #endif // GGML_OP_DIV
 
+#ifdef GGML_OP_ADD_BROADCAST
+
+void ggml_op_add_broadcast(const INPUT0_DTYPE * __restrict in0,
+                           const INPUT1_DTYPE * __restrict in1,
+                           OUTPUT_DTYPE * __restrict out,
+                           int32_t tile_size,
+                           int32_t tile_idx,
+                           int32_t ne0_src1) {
+    event0();
+
+    // Compute starting offset within src1 for this tile.
+    // global_offset = (tile_idx * tile_size) % ne0_src1
+    // To avoid 64-bit division, we compute: offset = (tile_idx % (ne0_src1/gcd)) * tile_size % ne0_src1
+    // For simplicity, we just track the running index within src1.
+    int32_t idx_src1 = (tile_idx * (tile_size % ne0_src1)) % ne0_src1;
+
+    for (int32_t i = 0; i < tile_size; ++i) {
+        out[i] = static_cast<OUTPUT_DTYPE>(
+            static_cast<float>(in0[i]) + static_cast<float>(in1[idx_src1])
+        );
+        // Increment and wrap
+        idx_src1++;
+        if (idx_src1 >= ne0_src1) {
+            idx_src1 = 0;
+        }
+    }
+
+    event1();
+}
+
+#endif // GGML_OP_ADD_BROADCAST
+
+#ifdef GGML_OP_SUB_BROADCAST
+
+void ggml_op_sub_broadcast(const INPUT0_DTYPE * __restrict in0,
+                           const INPUT1_DTYPE * __restrict in1,
+                           OUTPUT_DTYPE * __restrict out,
+                           int32_t tile_size,
+                           int32_t tile_idx,
+                           int32_t ne0_src1) {
+    event0();
+
+    int32_t idx_src1 = (tile_idx * (tile_size % ne0_src1)) % ne0_src1;
+
+    for (int32_t i = 0; i < tile_size; ++i) {
+        out[i] = static_cast<OUTPUT_DTYPE>(
+            static_cast<float>(in0[i]) - static_cast<float>(in1[idx_src1])
+        );
+        idx_src1++;
+        if (idx_src1 >= ne0_src1) {
+            idx_src1 = 0;
+        }
+    }
+
+    event1();
+}
+
+#endif // GGML_OP_SUB_BROADCAST
+
+#ifdef GGML_OP_MUL_BROADCAST
+
+void ggml_op_mul_broadcast(const INPUT0_DTYPE * __restrict in0,
+                           const INPUT1_DTYPE * __restrict in1,
+                           OUTPUT_DTYPE * __restrict out,
+                           int32_t tile_size,
+                           int32_t tile_idx,
+                           int32_t ne0_src1) {
+    event0();
+
+    int32_t idx_src1 = (tile_idx * (tile_size % ne0_src1)) % ne0_src1;
+
+    for (int32_t i = 0; i < tile_size; ++i) {
+        out[i] = static_cast<OUTPUT_DTYPE>(
+            static_cast<float>(in0[i]) * static_cast<float>(in1[idx_src1])
+        );
+        idx_src1++;
+        if (idx_src1 >= ne0_src1) {
+            idx_src1 = 0;
+        }
+    }
+
+    event1();
+}
+
+#endif // GGML_OP_MUL_BROADCAST
+
+#ifdef GGML_OP_DIV_BROADCAST
+
+void ggml_op_div_broadcast(const INPUT0_DTYPE * __restrict in0,
+                           const INPUT1_DTYPE * __restrict in1,
+                           OUTPUT_DTYPE * __restrict out,
+                           int32_t tile_size,
+                           int32_t tile_idx,
+                           int32_t ne0_src1) {
+    event0();
+
+    int32_t idx_src1 = (tile_idx * (tile_size % ne0_src1)) % ne0_src1;
+
+    for (int32_t i = 0; i < tile_size; ++i) {
+        out[i] = static_cast<OUTPUT_DTYPE>(
+            static_cast<float>(in0[i]) / static_cast<float>(in1[idx_src1])
+        );
+        idx_src1++;
+        if (idx_src1 >= ne0_src1) {
+            idx_src1 = 0;
+        }
+    }
+
+    event1();
+}
+
+#endif // GGML_OP_DIV_BROADCAST
+
 } // extern "C"
